40a255b518c618533a2b128081902e1678f8075f
adolfoillera <adolfoillera@gmail.com>
2026-08-20 19:00:34 +0200

    Committing 4 items from workspace 0c716bcd-563b-4126-9df7-bf03a76d8d99

---
+++ Gold.Warehouse/dbo/Tables/DimRLS_Usuarios.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [dbo].[DimRLS_Usuarios] (
+
+	[Email] varchar(100) NULL, 
+	[Region] varchar(100) NULL, 
+	[Pais] varchar(100) NULL, 
+	[UnidadNegocio] varchar(100) NULL, 
+	[Rol] varchar(50) NULL
+);
---
+++ Gold.Warehouse/dbo/Tables/Usuarios.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [dbo].[Usuarios] (
+
+	[Email] varchar(100) NULL, 
+	[Valor] int NULL, 
+	[Pais] varchar(50) NULL
+);
